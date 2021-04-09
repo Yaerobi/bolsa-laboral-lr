e607e795b54751bbbd8042efc078da7f0c9ee151
Emmanuel Arias <eamanu@yaerobi.com>
2021-04-09 20:37:34 -0300

    add DTO user

---
+++ app/main/utils/dto.py
@@ -0,0 +1,12 @@
+from flask_restplus import Namespace, fields
+
+
+class UserDto:
+    api = Namespace('user', description='user related operations')
+    user = api.model('user', {
+        'email': fields.String(required=True, description='user email address'),
+        'name': fields.String(required=True, description='user name'),
+        'surname': fields.String(required=True, description='user surname'),
+        'password': fields.String(required=True, description='user password'),
+        'user_id': fields.String(description='user Identifier')
+    })
